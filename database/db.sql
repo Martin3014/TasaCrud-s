7ffb4b28c572eadb9a9589dd943abe9a0a04ee98
Martin <3014683970101@ingenieria.usac.edu.gt>
2023-12-05 08:21:45 -0600

    Adapté la funcionalidad del proyecto para que funcione como un generador de tasas de cambio

--- database/db.sql
+++ database/db.sql
@@ -1,6 +1,6 @@
 -- Creación de base de datos
 
-CREATE DATABASE dbcrud;
+/*CREATE DATABASE dbcrud;
 
 use dbcrud;
 
@@ -9,6 +9,25 @@ CREATE TABLE Tasa (
     id INT(6) AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     description VARCHAR(100),
+    fecha DATE NOT NULL
+
+);
+SHOW TABLES;
+
+describe Tasa; */
+
+
+-- Creación de base de datos
+
+CREATE DATABASE dbcrud;
+
+use dbcrud;
+
+-- Tabla de Tasa de cambio
+CREATE TABLE Tasa (
+    id INT(6) AUTO_INCREMENT PRIMARY KEY,
+    colones DECIMAL(65, 2) NOT NULL,
+    usd DECIMAL(10, 2) NOT NULL,
     fecha DATE NOT NULL
 
 );
