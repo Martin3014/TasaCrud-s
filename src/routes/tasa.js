d65cc75efe3aad0f3ad3fb9161358bb0977ae4a7
Martin <3014683970101@ingenieria.usac.edu.gt>
2023-12-05 02:57:40 -0600

    Requerimientos básicos de la prueba funcionando y código documentado

--- src/routes/tasa.js
+++ src/routes/tasa.js
@@ -1,7 +1,11 @@
+//Rutas para la tasa de cambio
+
 const express = require ('express');
 const router = express.Router();
 
 const tasaController = require('../controllers/tasaController');
+
+//Rutas para el CRUD de la tasa de cambio
 
 router.get('/', tasaController.list);
 router.post('/add', tasaController.save);
@@ -9,5 +13,5 @@ router.get('/delete/:id', tasaController.delete);
 router.get('/update/:id', tasaController.update);
 router.post('/update/:id', tasaController.updateDatos);
 
-
+//Exportar las rutas
 module.exports = router;
